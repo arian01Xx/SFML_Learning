d87736aa0cff3bb01fe6757471a9d799918ee75a
arian01Xx <arian_joseph@outlook.es>
2026-02-12 22:18:55 -0500

    sfml

---
+++ main.cpp
@@ -0,0 +1,93 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <array>
+
+int main(){
+
+    //para crear una ventana
+    sf::RenderWindow window(sf::VideoMode({200, 200}), "SFML works!");
+    
+    //para crear un circulo y pintarlo
+    sf::CircleShape shape(50.f); 
+    /*PARA AGREGAR TEXTURA CON IMAGEN
+    shape.setTexture(&texture);
+    shape.setTextureRect(sf::IntRect({10,10},{100,100}));*/
+    shape.setFillColor(sf::Color::Green); //o coloreas con (sf::Color::Green);     
+    shape.setOutlineThickness(10.f);
+    shape.setPosition({100.f, 20.f});
+    shape.setOutlineColor(sf::Color::Blue);
+
+    //RECTANGULO
+    sf::RectangleShape rectangle({120.f, 50.f});
+    rectangle.setFillColor(sf::Color::Blue);
+    rectangle.setPosition({100.f, 120.f});
+    rectangle.setSize({100.f, 100.f});
+
+    //CIRCULO
+    sf::CircleShape circle(200.f);
+    circle.setPosition({100.f, 240.f});
+    circle.setFillColor(sf::Color::Red);
+    circle.setRadius(40.f);
+    circle.setPointCount(100);
+
+    //CREAR FIGURAS CON NUMERO DE LADO
+    sf::CircleShape triangle(80.f, 3);
+    triangle.setPosition({100.f, 300.f});
+    triangle.setFillColor(sf::Color::Green);
+    //triangle.setRadius(80.f);
+    //triangle.setPointCount(100);
+    
+    sf::CircleShape square(80.f, 4);
+    square.setPosition({100.f, 400.f});
+    square.setFillColor(sf::Color::Red);
+    //square.setRadius(80.f);
+    //square.setPointCount(100);
+
+    sf::CircleShape octagon(80.f, 8);
+    octagon.setPosition({50.f, 500.f});
+    octagon.setFillColor(sf::Color::Blue);
+    //octagon.setRadius(80.f);
+    //octagon.setPointCount(100);
+    
+    sf::ConvexShape convex;
+    convex.setPointCount(5);
+    convex.setPoint(0,{0.f, 0.f});
+    convex.setPoint(1,{150.f, 10.f,});
+    convex.setPoint(2,{120.f, 90.f});
+    convex.setPoint(3,{30.f, 100.f});
+    convex.setPoint(4,{0.f, 50.f});
+    convex.setFillColor(sf::Color::White);
+
+    sf::RectangleShape line({150.f, 5.f});
+    line.setFillColor(sf::Color::Red);
+    line.setPosition({40.f, 10.f});
+    line.rotate(sf::degrees(45));
+
+    std::array _line={
+        sf::Vertex{sf::Vector2f(30.f, 30.f)},
+        sf::Vertex{sf::Vector2f(150.f, 150.f)}
+    };
+
+    while (window.isOpen()){
+        while (const std::optional event = window.pollEvent()){
+            if (event->is<sf::Event::Closed>()) window.close();
+        }
+
+        window.clear(); //fondo negro
+
+        window.draw(shape); //para que dibuje las figuras creadas
+        window.draw(rectangle);
+        window.draw(circle);
+        window.draw(triangle);
+        window.draw(square);
+        window.draw(octagon);
+        window.draw(convex);
+        window.draw(line);
+        window.draw(_line.data(), _line.size(), sf::PrimitiveType::Lines);
+
+        window.display(); //finalizar el frame actual
+    }
+    return 0;
+}
+
+//g++ main.o -o main -lsfml-graphics -lsfml-window -lsfml-system
---
+++ main3.cpp
@@ -0,0 +1,78 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main(){
+    sf::RenderWindow window(sf::VideoMode({800,600}), "SFML");
+
+    sf::Vertex vertex;
+    vertex.position=sf::Vector2f(10.f, 50.f);
+    vertex.color=sf::Color::Green;
+    vertex.texCoords=sf::Vector2f(100.f, 100.f);
+    //sf::Vertex vertex{{10.f, 50.f}, sf::Color::Red, {100.f, 100.f}};
+   
+    sf::VertexArray triangle(sf::PrimitiveType::Triangles, 3);
+
+    triangle[0].position=sf::Vector2f(10.f, 10.f);
+    triangle[1].position=sf::Vector2f(100.f, 10.f);
+    triangle[2].position=sf::Vector2f(100.f, 100.f);
+
+    triangle[0].color=sf::Color::Red; //DEFINIMOS LOS COLORES POR CADA ESQUINA
+    triangle[1].color=sf::Color::Blue;
+    triangle[2].color=sf::Color::Green;
+
+    //--------------------------------------------------------------
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({100.f, 20.f});
+
+    //1. LOAD A FONT
+    sf::Font font; 
+    if(!font.openFromFile("arial.ttf")){
+        std::cerr<<"Error loading font file"<<std::endl;
+        return -1;
+    }
+
+    //2. CREATE A TEXT OBJECT AND SET ITS PROPERTIES
+    sf::Text text(font);
+    text.setString("ARIAN JOSEPH");
+    text.setCharacterSize(50);
+    text.setFillColor(sf::Color::Red);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    text.setPosition({100.f, 100.f});
+
+    while(window.isOpen()){
+        while(const std::optional event=window.pollEvent()){
+            if(event->is < sf::Event::Closed>()) window.close();
+        }
+
+        window.clear();
+
+        window.draw(shape);
+        window.draw(text);
+        window.draw(triangle);
+
+        window.display();
+
+    }
+
+    return 0;
+}
+
+//tambien tiene para sf::Text
+/*
+ std::vector<sf::Vertex> vertices;
+vertices.push_back(sf::Vertex{...});
+...
+
+window.draw(vertices.data(), vertices.size(), sf::PrimitiveType::Triangles);
+
+----------------------------------------------------------------------------
+std::array vertices = {
+    sf::Vertex{...},
+    sf::Vertex{...}
+};
+
+window.draw(vertices.data(), vertices.size(), sf::PrimitiveType::Lines);
+ * */
+
+
